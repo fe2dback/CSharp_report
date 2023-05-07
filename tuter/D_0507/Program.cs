a42df3b9c68c3eccc7c236e2746afdce3fa1df76
fe2dback <66859338+fe2dback@users.noreply.github.com>
2023-05-07 20:59:59 +0900

    0507

---
+++ tuter/D_0507/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace D_0507
+{
+    enum ClassType
+    {
+        None = 0,
+        knight = 1,
+        Archer = 2,
+        Magician = 3,
+    }
+
+    
+
+    internal class main
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            ChooseClass();
+
+        }
+
+        #region choose Char
+        static ClassType ChooseClass()
+        {
+            
+            Console.Write("직업선택 : ");
+            string input = Console.ReadLine();
+            ClassType playerClass = new ClassType();
+
+            switch (input)
+            {
+                case "1":
+                    playerClass = ClassType.knight;
+                    Console.Write($"직업은 : {playerClass}");
+                    break;
+                case "2":
+                    playerClass = ClassType.Archer;
+                    Console.Write($"직업은 : {playerClass}");
+                    break;
+                case "3":
+                    playerClass = ClassType.Magician;
+                    Console.Write($"직업은 : {playerClass}");
+                    break;
+                default:
+                    playerClass = ClassType.None;
+                    Console.WriteLine("지정되지 않은 값");
+                    break;
+            }
+            return playerClass;
+        }
+        #endregion
+    }
+
+}
