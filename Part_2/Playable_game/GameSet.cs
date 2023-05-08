9e29c7b5d017e4743db2055525179ce41f03c73f
fe2dback <66859338+fe2dback@users.noreply.github.com>
2023-05-09 08:23:30 +0900

    0509
    distance check

---
+++ Part_2/Playable_game/GameSet.cs
@@ -0,0 +1,195 @@
+﻿using System;
+//2차원 배열을 이용하여 아래 영상과 같이 랜덤 아이템 10개를 (50,20)화면에 배치하고 
+//플레이어(P)가 방향키로 돌아다니면서 다 먹으면 클리어 하는 게임 제작(플러에어가 화면을 벗어나면 반대방향에서 나오기)
+namespace Playable_game
+{
+
+
+    class onGame
+    {
+        #region value
+        static int player_start_x = 5;
+        static int player_start_y = 5;
+        static int Goal = 10;
+        static int Score = 0;
+        static int onboard_x = 50;
+        static int onboard_y = 20; 
+        static int player_pos_x;
+        static int player_pos_y;
+        static bool[,] board = new bool[onboard_x, onboard_y];
+        static bool replay = false;
+        static bool playing = true;
+        static bool dead = false;
+        #endregion
+
+        #region 시작옵션
+        static void start_opt()
+        {
+            map();
+            player_pos_x = player_start_x;
+            player_pos_y = player_start_y;
+            Console.SetCursorPosition(player_pos_x, player_pos_y);
+            Console.Write("P");
+        }
+        #endregion
+
+        #region 시작
+        public void start()
+        {
+
+            while (playing == true)
+            {
+                Playing();
+            }
+        }
+        #endregion
+
+        #region 게임 플레이
+        static void Playing()
+        {
+            start_opt();
+            while (true)
+            {
+                
+                player_move();
+                player_check();
+                Console.SetCursorPosition(50, 5);
+                Console.Write($"{player_pos_x},{player_pos_y}");
+                Console.SetCursorPosition(50, 6);
+                Console.Write(Score);
+
+
+
+                if (Score == Goal)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Clear");
+                    Console.WriteLine("Replay : R");
+                    dead = true;
+                    playing = false;
+                    replay = true;
+                    Score = 0;
+                }
+            }
+        }
+        #endregion
+
+        #region 점수관리
+        static void score()
+        {
+            if (board[player_pos_x, player_pos_y] == true)
+            {
+                Score++;
+                board[player_pos_x, player_pos_y] = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region 범위 체크
+        static void player_check()
+        {
+            
+
+        }
+        #endregion
+
+        #region 플레이어 조작
+        static void player_move()
+        {
+            ConsoleKeyInfo player_key = Console.ReadKey();
+
+            switch (player_key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (dead == false)
+                    {
+                        player_pos_y--;
+                        Console.SetCursorPosition(player_pos_x, player_pos_y+1);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(player_pos_x, player_pos_y);
+                        Console.Write("P");
+                        score();
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (dead == false)
+                    {
+                        player_pos_y++;
+                        Console.SetCursorPosition(player_pos_x, player_pos_y-1);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(player_pos_x, player_pos_y);
+                        Console.Write("P");
+                        score();
+                    }
+                        break;
+                case ConsoleKey.LeftArrow:
+                    if (dead == false)
+                    {
+                        player_pos_x--;
+                        Console.SetCursorPosition(player_pos_x+1, player_pos_y);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(player_pos_x, player_pos_y);
+                        Console.Write("P");
+                        score();
+                    }
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    if (dead == false)
+                    {
+                        player_pos_x++;
+                        Console.SetCursorPosition(player_pos_x, player_pos_y);
+                        Console.Write("P");
+                        Console.SetCursorPosition(player_pos_x-1, player_pos_y);
+                        Console.Write(" ");
+                        score();
+                    }
+                    break;
+                case ConsoleKey.R:
+
+                    if (replay == true)
+                    {
+                        Console.Clear();
+                        playing = true;
+                        replay = false;
+                        dead = false;
+                        onGame fe = new onGame();
+                        fe.start();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        #endregion
+
+        #region 아이템 생성
+        static void map()
+        {
+
+            Random random = new Random();
+            int Pos_1, Pos_2;
+            for (int i = 0; i < Goal; i++)
+            {
+                Pos_1 = random.Next(0, onboard_x);
+                Pos_2 = random.Next(0, onboard_y);
+                Console.SetCursorPosition(Pos_1, Pos_2);
+                board[Pos_1, Pos_2] = true;
+                if (board[Pos_1, Pos_2] == true)
+                {
+                    Console.Write("$");
+                }
+
+            }
+
+        }
+        #endregion
+    }
+
+}
+
---
+++ Part_2/Playable_game/Manager.cs
@@ -0,0 +1,15 @@
+﻿namespace Playable_game
+{
+    class Manager
+    {
+        static void Main(string[] args)
+        {
+            onGame play = new onGame();
+            while (true)
+            {
+                play.start();
+            }
+            
+        }
+    }
+}
