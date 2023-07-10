482059d853ae09329cf9bf4b4b6608d37d7a2abf
fe2dback <xnqppskxn@naver.com>
2023-07-11 02:00:02 +0900

    zz

---
+++ Part_3/bbs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class bubbbbbbblesort
+{
+    static void Main(string[] args)
+    {
+        int size = 10;
+        int[] numbers = new int[size];
+        make_nums(size, numbers);
+    }
+
+    static void sort(int[] number)
+    {
+        int n = number.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (number[j] > number[j + 1])
+                {
+                    int temp = number[j];
+                    number[j] = number[j + 1];
+                    number[j + 1] = temp;
+                }
+            }
+        }
+        foreach(int num in number)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    static void make_nums(int size, int[] numbers)
+    {
+        Random random = new Random();
+        for(int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(0,99);    
+        }
+        sort(numbers);
+
+    }
+}
---
+++ Part_3/wordrow/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            string text = Console.ReadLine();
+
+            compare(text);
+        }
+    }
+
+    static void compare(string text)
+    {
+        char[] type = new char[text.Length];
+        bool odd = false;
+        bool even = false;
+
+        if (text.Length % 2 == 0)
+        {
+            even = true;
+        }
+        else
+        {
+            odd = true;
+        }
+
+        for (int i = 0; i < type.Length; i++)
+        {
+            type[i] = text[i];
+        }
+
+        if (even == true)
+        {
+            int i = 0;
+            while (i < type.Length / 2)
+            {
+                if (type[i] == type[type.Length - 1 - i])
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                    return;
+                }
+            }
+            Console.WriteLine("true");
+            even = false;
+        }
+
+        if (odd == true)
+        {
+            int i = 0;
+            while (i < type.Length / 2)
+            {
+                if (type[i] == type[type.Length - 1 - i])
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                    return;
+                }
+            }
+            Console.WriteLine("true");
+            odd = false;
+        }
+
+    }
+}
